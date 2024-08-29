8f8e1ea3c71aa4e19e0fa6c06c7fd5dd582b8ec4
Minjoo <fudnr4010@naver.com>
2024-08-29 21:40:32 +0900

    refactor : Use Optional Admin and User entities to improve null safety

--- src/main/java/com/studyshop/shop/AdminRepository.java
+++ src/main/java/com/studyshop/shop/AdminRepository.java
@@ -2,6 +2,8 @@ package com.studyshop.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
-    Admin findById(String id);  // 관리자 id로 특정 관리자 조회
+    Optional<Admin> findById(String id);  // 관리자 id로 특정 관리자 조회(NPE를 방지)
 }
--- src/main/java/com/studyshop/shop/UserRepository.java
+++ src/main/java/com/studyshop/shop/UserRepository.java
@@ -2,6 +2,8 @@ package com.studyshop.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
-    User findById(String id);  // User 테이블의 id 필드로 특정 사용자 조회
+    Optional<User> findById(String id);  // User 테이블의 id 필드로 특정 사용자 조회 (NPE를 방지)
 }
