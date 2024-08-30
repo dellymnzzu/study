86f8103c6f3c74b2eaf05f817fd6d0106942d2e1
Minjoo <fudnr4010@naver.com>
2024-08-30 23:58:46 +0900

    refactor : Rename BasicController to UserController

---
+++ src/main/java/com/studyshop/shop/ProductController.java
@@ -0,0 +1,2 @@
+package com.studyshop.shop;public class ProductController {
+}
--- src/main/java/com/studyshop/shop/UserController.java
+++ src/main/java/com/studyshop/shop/UserController.java
@@ -4,13 +4,10 @@ package com.studyshop.shop;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-
 
 
 @Controller
-public class BasicController {
+public class UserController {
     @GetMapping("/list")
     String hello(Model model){
         model.addAttribute("name","홍길동");
